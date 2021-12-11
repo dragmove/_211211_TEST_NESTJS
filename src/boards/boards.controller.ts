b9effbd5fdc8851970248dd6742a2bb5d502ac04
hyunseok.kim <hyunseok.kim@42dot.ai>
2021-12-11 20:38:35 +0900

    feat: Add CreateBoardDto

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
-import { title } from 'process';
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
@@ -13,10 +13,18 @@ export class BoardsController {
   }
 
   @Post('/')
+  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+    return this.boardsService.createBoard(createBoardDto);
+  }
+  /*
+  // TODO: Remove. 
+  // Use service with no dto definition
+  @Post('/')
   createBoard(
     @Body('title') title: string,
     @Body('description') description: string,
   ): Board {
     return this.boardsService.createBoard(title, description);
   }
+  */
 }
