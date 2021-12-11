5237e38c7e2970c6271932fe706c24977bd67167
hyunseok.kim <hyunseok.kim@42dot.ai>
2021-12-11 20:55:40 +0900

    feat: Add getBoardById service

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -12,19 +12,13 @@ export class BoardsController {
     return this.boardsService.getAllBoards();
   }
 
+  @Get('/:id')
+  getBoardById(@Param('id') id: string): Board {
+    return this.boardsService.getBoardById(id);
+  }
+
   @Post('/')
   createBoard(@Body() createBoardDto: CreateBoardDto): Board {
     return this.boardsService.createBoard(createBoardDto);
   }
-  /*
-  // TODO: Remove. 
-  // Use service with no dto definition
-  @Post('/')
-  createBoard(
-    @Body('title') title: string,
-    @Body('description') description: string,
-  ): Board {
-    return this.boardsService.createBoard(title, description);
-  }
-  */
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -15,13 +15,18 @@ export class BoardsService {
     const { title, description } = createBoardDto;
 
     const board: Board = {
-      id: uuidv1(),
+      id: uuidv1(new Date()),
       title,
       description,
       status: BoardStatus.PUBLIC,
     };
     this.boards.push(board);
 
+    return board;
+  }
+
+  getBoardById(id: string): Board {
+    const board: Board = this.boards.find((board) => board.id === id);
     return board;
   }
 }
