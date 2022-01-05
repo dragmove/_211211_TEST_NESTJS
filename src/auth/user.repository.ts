608860a65ac0a626983aef46a3d843fc57b10544
hyunseok.kim <hyunseok.kim@42dot.ai>
2022-01-05 10:02:34 +0900

    feat: Encode password with bcryptjs

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -5,15 +5,19 @@ import {
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcryptjs';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
     const { username, password } = authCredentialDto;
 
+    const salt: string = await bcrypt.genSalt();
+    const hashedPassword: string = await bcrypt.hash(password, salt);
+
     const user = this.create({
       username,
-      password,
+      password: hashedPassword,
     });
 
     try {
